830c4208bca383367fe9c26c2ff24eb6daea3ebd
Như Huy Nguyễn <nhuhuy1999@gmail.com>
2023-10-23 22:53:19 +0700

    Category Controller added

---
+++ app/Http/Controllers/admin/categoriesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use \Str;
+use \DB;
+
+class categoriesController extends Controller
+{
+    public function create()
+    {
+        return view("admin.categories.create");
+    }
+    
+    public function store(Request $request)
+    {
+        $category['name'] = $request->name;
+        $category['created_at'] = now();
+        $category['updated_at'] = now();
+        $category['createdBy'] = '1';
+        $category['imageUrl'] = 'abc.png';
+        $category['slug'] = Str::slug($request->name);
+
+        DB::table('categories')->insert($category);
+
+        return redirect()->to('/admin/categories');
+    }
+
+    public function index()
+    {
+        return view('admin.categories.index');
+    }
+}
+
+// Bài Tập:
+// Tạo Admin
+// Tạo Post
+// Tạo Tag
+// Tìm hiểu cách hiển thị dữ liệu ra
+// DB::table(name)->get() => Lấy data
+// gợi ý Truyền data sang view index
+// foreach để hiển thị
