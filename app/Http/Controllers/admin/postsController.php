974d6e97faaa0a40f8b515b4353c378a46e9c674
Như Huy Nguyễn <nhuhuy1999@gmail.com>
2023-10-24 23:34:13 +0700

    add some touch to controller

--- app/Http/Controllers/admin/postsController.php
+++ app/Http/Controllers/admin/postsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \Str;
-use \DB;
+use Illuminate\Support\Facades\DB;
 
 class postsController extends Controller
 {
