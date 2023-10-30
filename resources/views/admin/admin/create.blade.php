f94be8333678f5fa87141eeec4c2c5dd0b25f65d
Như Huy Nguyễn <nhuhuy1999@gmail.com>
2023-10-30 22:42:20 +0700

    change something

--- resources/views/admin/admin/create.blade.php
+++ resources/views/admin/admin/create.blade.php
@@ -10,15 +10,15 @@
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1">Email</label>
-      <input type="text" class="form-control" id="name"  name="email" >
+      <input type="text" class="form-control" id="email"  name="email" >
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1">Password</label>
-      <input type="password" class="form-control" id="name"  name="password" >
+      <input type="password" class="form-control" id="password"  name="password" >
     </div>
     <div class="form-group">
       <label for="avatar">Avatar</label>
-      <input type="file" class="form-control" id="name"  name="avatar" >
+      <input type="file" class="form-control" id="avatar"  name="avatar" >
     </div>
         <button type="submit" class="btn btn-primary">Submit</button>
   </form>
